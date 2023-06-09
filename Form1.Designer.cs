4060b3bb8f41f0e3576b82d0838e791607b1d84f
ace <114324924+gureett@users.noreply.github.com>
2023-06-08 21:52:23 -0400

    Reworked program in C# using Visual Studio

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CardInfoGrabber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cardNumberTextBox = new System.Windows.Forms.TextBox();
+            this.resultTextBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cardNumberTextBox
+            // 
+            this.cardNumberTextBox.Location = new System.Drawing.Point(12, 22);
+            this.cardNumberTextBox.Name = "cardNumberTextBox";
+            this.cardNumberTextBox.Size = new System.Drawing.Size(233, 20);
+            this.cardNumberTextBox.TabIndex = 0;
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(12, 48);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.Size = new System.Drawing.Size(324, 82);
+            this.resultTextBox.TabIndex = 1;
+            this.resultTextBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter the Card Number Here:";
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(252, 22);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(84, 19);
+            this.checkButton.TabIndex = 3;
+            this.checkButton.Text = "Grab";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 142);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.cardNumberTextBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Card Info Grabber";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox cardNumberTextBox;
+        private System.Windows.Forms.RichTextBox resultTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button checkButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardInfoGrabber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            resultTextBox.Text = string.Empty;
+            string cardNumber = cardNumberTextBox.Text.Trim();
+
+            if (IsValidCardNumber(cardNumber))
+            {
+                CardInfo cardInfo = GetCardInfo(cardNumber);
+                if (cardInfo != null)
+                {
+                    resultTextBox.Text = cardInfo.ToString();
+                }
+                else
+                {
+                    resultTextBox.Text = "Unable to retrieve card information";
+                }
+            }
+            else
+            {
+                resultTextBox.Text = "Invalid card number";
+            }
+        }
+
+        private bool IsValidCardNumber(string cardNumber)
+        {
+            string sanitizedCardNumber = new string(cardNumber.ToCharArray()
+                .Where(c => char.IsDigit(c)).ToArray());
+
+            if (sanitizedCardNumber.Length < 12 || sanitizedCardNumber.Length > 19)
+            {
+                return false;
+            }
+
+            int sum = 0;
+            bool alternate = false;
+
+            for (int i = sanitizedCardNumber.Length - 1; i >= 0; i--)
+            {
+                int digit = int.Parse(sanitizedCardNumber[i].ToString());
+
+                if (alternate)
+                {
+                    digit *= 2;
+                    if (digit > 9)
+                    {
+                        digit = (digit % 10) + 1;
+                    }
+                }
+
+                sum += digit;
+                alternate = !alternate;
+            }
+
+            return sum % 10 == 0;
+        }
+
+        private CardInfo GetCardInfo(string cardNumber)
+        {
+            string bin = cardNumber.Substring(0, 6);
+            string apiUrl = "https://lookup.binlist.net/" + bin;
+
+            try
+            {
+                WebRequest request = WebRequest.Create(apiUrl);
+                request.Method = "GET";
+
+                using (WebResponse response = request.GetResponse())
+                using (Stream responseStream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(responseStream, Encoding.UTF8))
+                {
+                    string jsonResponse = reader.ReadToEnd();
+                    JObject jsonObject = JObject.Parse(jsonResponse);
+
+                    CardInfo cardInfo = new CardInfo();
+                    cardInfo.CardType = jsonObject["type"].ToString();
+                    cardInfo.Bank = jsonObject["bank"]["name"].ToString();
+                    cardInfo.Country = jsonObject["country"]["name"].ToString();
+                    cardInfo.Scheme = jsonObject["scheme"].ToString();
+                    cardInfo.Brand = jsonObject["brand"].ToString();
+
+                    return cardInfo;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        private class CardInfo
+        {
+            public string CardType { get; set; }
+            public string Bank { get; set; }
+            public string Country { get; set; }
+            public string Scheme { get; set; }
+            public string Brand { get; set; }
+
+            public override string ToString()
+            {
+                return $"Card Type: {CardType}\n" +
+                       $"Bank: {Bank}\n" +
+                       $"Country: {Country}\n" +
+                       $"Scheme: {Scheme}\n" +
+                       $"Brand: {Brand}";
+            }
+        }
+    }
+}
